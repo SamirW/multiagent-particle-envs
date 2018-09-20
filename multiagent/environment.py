a4eddfd7341ac40205fb15035218923cbc8de4e0
Samir Wadhwania <swadhwania96@gmail.com>
2018-09-20 13:20:29 -0400

    num agents

--- multiagent/environment.py
+++ multiagent/environment.py
@@ -13,7 +13,7 @@ class MultiAgentEnv(gym.Env):
 
     def __init__(self, world, reset_callback=None, reward_callback=None,
                  observation_callback=None, info_callback=None,
-                 done_callback=None, shared_viewer=True):
+                 done_callback=None, shared_viewer=True, discrete_action_input=False):
 
         self.world = world
         self.agents = self.world.policy_agents
@@ -28,7 +28,7 @@ class MultiAgentEnv(gym.Env):
         # environment parameters
         self.discrete_action_space = True
         # if true, action is a number 0...N, otherwise action is a one-hot N-dimensional vector
-        self.discrete_action_input = True #False
+        self.discrete_action_input = discrete_action_input #False
         # if true, even the action is continuous, action will be performed discretely
         self.force_discrete_action = world.discrete_action if hasattr(world, 'discrete_action') else False
         # if true, every agent has the same reward
@@ -76,6 +76,9 @@ class MultiAgentEnv(gym.Env):
         else:
             self.viewers = [None] * self.n
         self._reset_render()
+
+    def seed(self, seed):
+        np.random.seed(seed)
 
     def step(self, action_n):
         obs_n = []
